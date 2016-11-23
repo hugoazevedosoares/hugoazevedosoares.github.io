82ceccc96841c39ac17d95d8b8efd6fdcc276492
hugoazevedosoares <hugo.soares@outlook.com>
2016-11-22 23:24:23 -0200

    Images added

--- js/main.js
+++ js/main.js
@@ -1,43 +1,45 @@
-$('document').ready(function() {
-  //$('.box').fadeIn(2000);
+$('document').ready(function () {
+    //Fade all page in
+    //$('.box').fadeIn(2000);
 
-$('#name').animate({opacity:1},900, function() {
-    showStudent();
-  });  
-  $('#contact').animate({opacity:1},900, function() {
-    showCell();
-  });
-  
+    //Fade text by text in
+    $('#name').animate({ opacity: 1 }, 900, function () {
+        showStudent();
+    });
+    $('#contact').animate({ opacity: 1 }, 900, function () {
+        showCell();
+    });
+    
 });
 
-var showCell = function() {
-    $('#cell').animate({opacity:1},700, function() {
-        showBar();
-    });
-}
-
-var showBar = function() {
-    $('#bar').animate({opacity:1},700, function() {
+var showCell = function () {
+    $('#cell').animate({ opacity: 1 }, 700, function () {
         showEmail();
     });
 }
 
-var showEmail = function() {
-    $('#email').animate({opacity:1},700);
+var showEmail = function () {
+    $('#email').animate({ opacity: 1 }, 700, function() {
+        showSocial();
+    });
 }
 
-var showStudent = function() {
-    $('#student').animate({opacity:1},700, function() {
+var showSocial = function () {
+    $('#social').animate({ opacity: 1 }, 700);
+}
+
+var showStudent = function () {
+    $('#student').animate({ opacity: 1 }, 700, function () {
         showJob();
     });
 }
 
-var showJob = function() {
-    $('#job').animate({opacity:1},700, function() {
+var showJob = function () {
+    $('#job').animate({ opacity: 1 }, 700, function () {
         showHobby();
     });
 }
 
-var showHobby = function() {
-    $('#hobby').animate({opacity:1},700);
+var showHobby = function () {
+    $('#hobby').animate({ opacity: 1 }, 700);
 }
