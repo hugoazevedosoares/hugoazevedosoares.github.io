b3f216f9ca46b73ce0a8435fc0118f458314d7b8
hugoazevedosoares <hugo.soares@outlook.com>
2016-11-22 22:21:24 -0200

    Animations ok!

--- js/main.js
+++ js/main.js
@@ -1,3 +1,43 @@
-$('document').ready(function() {    
-    $('.box').fadeIn(2000);
+$('document').ready(function() {
+  //  $('.box').fadeIn(2000);
+  //$(this).find("span").animate({opacity:1},2000);
+  //$(this).getElementById('cell').animate({opacity:1},2000);
+  $('#name').animate({opacity:1},900, function() {
+    showStudent();
+  });  
+  $('#contact').animate({opacity:1},900, function() {
+    showCell();
+  });  
 });
+
+var showCell = function() {
+    $('#cell').animate({opacity:1},700, function() {
+        showBar();
+    });
+}
+
+var showBar = function() {
+    $('#bar').animate({opacity:1},700, function() {
+        showEmail();
+    });
+}
+
+var showEmail = function() {
+    $('#email').animate({opacity:1},700);
+}
+
+var showStudent = function() {
+    $('#student').animate({opacity:1},700, function() {
+        showJob();
+    });
+}
+
+var showJob = function() {
+    $('#job').animate({opacity:1},700, function() {
+        showHobby();
+    });
+}
+
+var showHobby = function() {
+    $('#hobby').animate({opacity:1},700);
+}
