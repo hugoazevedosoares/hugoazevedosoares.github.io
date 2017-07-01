182999344ee5e09a81d3d431f41f33a0cd9dea17
HugoS <hugo.soares@outlook.com>
2017-07-01 13:36:26 -0300

    modificações para Stilingue, sétimo período e background com degradê

--- js/main.js
+++ js/main.js
@@ -1,45 +1,44 @@
-$('document').ready(function () {
+var time = 700;
+
+$('document').ready(function() {
     //Fade all page in
     //$('.box').fadeIn(2000);
 
     //Fade text by text in
-    $('#name').animate({ opacity: 1 }, 900, function () {
+    $('#name').animate({
+        opacity: 1
+    }, 900, function() {
         showStudent();
     });
-    $('#contact').animate({ opacity: 1 }, 900, function () {
-        showCell();
-    });
-    
+
 });
 
-var showCell = function () {
-    $('#cell').animate({ opacity: 1 }, 700, function () {
-        showEmail();
-    });
-}
-
-var showEmail = function () {
-    $('#email').animate({ opacity: 1 }, 700, function() {
-        showSocial();
-    });
-}
-
-var showSocial = function () {
-    $('#social').animate({ opacity: 1 }, 700);
-}
-
-var showStudent = function () {
-    $('#student').animate({ opacity: 1 }, 700, function () {
+var showStudent = function() {
+    $('#student').animate({
+        opacity: 1
+    }, time, function() {
         showJob();
     });
 }
 
-var showJob = function () {
-    $('#job').animate({ opacity: 1 }, 700, function () {
+var showJob = function() {
+    $('#job').animate({
+        opacity: 1
+    }, time, function() {
         showHobby();
     });
 }
 
-var showHobby = function () {
-    $('#hobby').animate({ opacity: 1 }, 700);
+var showHobby = function() {
+    $('#hobby').animate({
+        opacity: 1
+    }, time, function() {
+        showContact();
+    });
+}
+
+var showContact = function() {
+    $('#contact').animate({
+        opacity: 1
+    }, time);
 }
